4fffee1b3ee30cb9c789b6eb6d6872cd6d5d2b12
n-thony <antoine.ntalumeso@gmail.com>
2024-03-09 13:42:45 +0300

    Added more tests

--- CandyCurshTest/CandyCrushUnitTest.cs
+++ CandyCurshTest/CandyCrushUnitTest.cs
@@ -17,7 +17,7 @@ namespace CandyCurshTest
             gameBoard.InitializeBoard();
 
             // Assert
-           // Assert.All(gameBoard.candies, row => Assert.All(row, candy => Assert.Null(candy)));
+            Assert.All(gameBoard.candies.Cast<ICandy>(), candy => Assert.NotNull(candy));
         }
 
         [Fact]
@@ -35,6 +35,60 @@ namespace CandyCurshTest
             // Assert
             Assert.Equal(targetCandy, gameBoard.candies[0, 0]);
             Assert.Equal(initialCandy, gameBoard.candies[0, 1]);
+        }
+
+        [Fact]
+        public void CheckForMatch_ShouldReturnTrue_HorizontalMatch()
+        {
+            // Arrange
+            const int rows = 5;
+            const int cols = 5;
+            GameBoard gameBoard = new GameBoard(rows, cols);
+            gameBoard.candies[0, 0] = new NormalCandy(CandyType.Red);
+            gameBoard.candies[0, 1] = new NormalCandy(CandyType.Red);
+            gameBoard.candies[0, 2] = new NormalCandy(CandyType.Red);
+
+            // Act
+            bool result = gameBoard.CheckForMatch(0, 0);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CheckForMatch_ShouldReturnTrue_VerticalMatch()
+        {
+            // Arrange
+            const int rows = 5;
+            const int cols = 5;
+            GameBoard gameBoard = new GameBoard(rows, cols);
+            gameBoard.candies[0, 0] = new NormalCandy(CandyType.Blue);
+            gameBoard.candies[1, 0] = new NormalCandy(CandyType.Blue);
+            gameBoard.candies[2, 0] = new NormalCandy(CandyType.Blue);
+
+            // Act
+            bool result = gameBoard.CheckForMatch(0, 0);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CheckForMatch_ShouldReturnFalse_NoMatch()
+        {
+            // Arrange
+            const int rows = 5;
+            const int cols = 5;
+            GameBoard gameBoard = new GameBoard(rows, cols);
+            gameBoard.candies[0, 0] = new NormalCandy(CandyType.Green);
+            gameBoard.candies[0, 1] = new NormalCandy(CandyType.Blue);
+            gameBoard.candies[0, 2] = new NormalCandy(CandyType.Red);
+
+            // Act
+            bool result = gameBoard.CheckForMatch(0, 0);
+
+            // Assert
+            Assert.False(result);
         }
     }
 
